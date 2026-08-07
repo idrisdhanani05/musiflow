164e7991a76c9c4e7d3f48c48d971f034e5c6dff
Idris Dhanani <idris.dhanani05@gmail.com>
2026-08-08 02:36:12 +0530

    Updated Spotify Clone with landing page and new pages

---
+++ js/login.js
@@ -0,0 +1,9 @@
+const loginForm = document.getElementById("loginForm");
+
+loginForm.addEventListener("submit", function(e){
+
+    e.preventDefault();
+
+    window.location.href="home.html";
+
+});
